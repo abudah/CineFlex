d46c89d6e38d3fa29a39db7813e153300d053db5
Fekede Alemayehu <fekedealemayehu4@gmail.com>
2023-10-13 13:36:39 +0300

    Add: add entities to the domain project

---
+++ Domain/Cinema.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Cinema : BaseEntity
+    {
+        public string Name { get; set; } = "";
+        public string Address { get; set; } = "";
+        public string City { get; set; } = "";
+        public string State { get; set; } = "";
+        public string Country { get; set; } = "";
+        public int Capacity { get; set; }
+
+    }
+}
---
+++ Domain/Movie.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Domain
+{
+    public class Movie : BaseEntity
+    {
+        public string Title { get; set; } = "";
+        public DateTime Release_date { get; set; }
+        public int Duration { get; set; }
+        public string Genre { get; set; } = "";
+        public string Director { get; set; } = "";
+        public float Rating { get; set; }
+
+    }
+}
